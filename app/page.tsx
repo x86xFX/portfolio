08b5cec55f69d09626b9959a4b377f7fcf4eb750
Theekshana Nirmana <101990722+x86xFX@users.noreply.github.com>
2024-07-27 17:13:28 +0530

    Accessibility issues fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import {Hero} from "@/app/partials/Hero";
-import {ProjectList} from "@/app/partials/ProjectList";
 import {redirect} from "next/navigation";
 
 export default function Home() {
   return (
       redirect('https://shattereddisk.github.io/rickroll/rickroll.mp4')
-      // <div>
-      //     <Hero/>
-      //     <ProjectList/>
-      // </div>
   );
 }
