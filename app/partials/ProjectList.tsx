6113199b42e53dcccb129230c072cdf777841b72
Theekshana Nirmana <101990722+x86xFX@users.noreply.github.com>
2024-09-27 07:55:36 +0530

    MemoX App added to ProjectList

--- app/partials/ProjectList.tsx
+++ app/partials/ProjectList.tsx
@@ -16,10 +16,7 @@ const ProjectList = () => (
                 name="Spark Music Player"
                 description="Unleash your personal soundtrack with Spark Music Player. This powerful music app lets you listen to your favorite songs, anytime, anywhere, without burning through data."
                 link="https://github.com/x86xFX/SparkMusicPlayer"
-                img={{
-                    src: './assets/images/sound-waves.png',
-                    alt: 'Spark Music Player',
-                }}
+                img={{src: './assets/images/sound-waves.png', alt: 'Spark Music Player'}}
                 category={
                     <>
                         <Tags color={ColorTags.LIME}>Android Development</Tags>
@@ -36,7 +33,7 @@ const ProjectList = () => (
                 name="Spark Spotify Artist API"
                 description="HTTP server that fetch artist's profile details from Spotify API. Written in Kotlin using Ktor Framework."
                 link="https://github.com/x86xFX/SparkSpotifyArtistAPI"
-                img={{src: './assets/images/api.png', alt: 'Project Fire'}}
+                img={{src: './assets/images/api.png', alt: 'Project SparkSpotifyArtistAPI'}}
                 category={
                     <>
                         <Tags color={ColorTags.EMERALD}>API Development</Tags>
@@ -50,12 +47,29 @@ const ProjectList = () => (
                 name="Notification Recorder App"
                 description="Real-time capture of incoming notifications using NotificationListenerService."
                 link="https://github.com/x86xFX/NotificationRecorder"
-                img={{src: './assets/images/notification.png', alt: 'Project Fire'}}
+                img={{src: './assets/images/notification.png', alt: 'Project NotificationRecorder'}}
                 category={
                     <>
                         <Tags color={ColorTags.LIME}>Android Development</Tags>
                         <Tags color={ColorTags.VIOLET}>Kotlin</Tags>
                         <Tags color={ColorTags.SKY}>Jetpack Compose</Tags>
+                        <Tags color={ColorTags.ROSE}>Material 3</Tags>
+                        <Tags color={ColorTags.ZINC}>Github</Tags>
+                    </>
+                }
+            />
+            <Project
+                name="MemoX"
+                description="Note-taking app with addtional location marking and photo picking features"
+                link="https://github.com/x86xFX/MemoX"
+                img={{src: './assets/images/note_app.png', alt: 'Project MemoX'}}
+                category={
+                    <>
+                        <Tags color={ColorTags.LIME}>Android Development</Tags>
+                        <Tags color={ColorTags.VIOLET}>Kotlin</Tags>
+                        <Tags color={ColorTags.SKY}>Jetpack Compose</Tags>
+                        <Tags color={ColorTags.FUCHSIA}>Google Maps</Tags>
+                        <Tags color={ColorTags.GREEN}>CameraX</Tags>
                         <Tags color={ColorTags.ROSE}>Material 3</Tags>
                         <Tags color={ColorTags.ZINC}>Github</Tags>
                     </>
